51a395056f1a07c1c873ef7d4fec2949c140169a
Cagatay Yucelen <cagatayyucelen@gmail.com>
2019-10-10 02:14:44 +0300

    tidy up

--- matcher.go
+++ matcher.go
@@ -8,67 +8,22 @@ import (
 	"time"
 )
 
-const (
-	weekdaysAbv   = "((Mon)|(Tue)|(Wed)|(Thu)|(Fri)|(Sat)|(Sun))"
-	weekdays      = "((Monday)|(Tuesday)|(Wednesday)|(Thursday)|(Friday)|(Saturday)|(Sunday))"
-	monthsAbv     = "((Jan)|(Feb)|(Mar)|(Apr)|(May)|(Jun)|(Jul)|(Aug)|(Sep)|(Oct)|(Nov)|(Dec))"
-	numericmonths = "(0[1-9]|1[0-2])"
-	days          = "((_[1-9])|([1-2][0-9])|3[01])"               // _1 - 31
-	daysWithZero  = "((0[1-9])|([1-2][0-9])|3[01])"               // 01 - 31
-	hhmmss        = "(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])" // 22:15:02
-	hhmm          = "(([0-1][0-9]|2[0-3]):[0-5][0-9])"            // 22:15
-	year          = "[0-9]{4}"                                    // 2006
-	timezone      = "[A-Z]{3}"                                    // MST
-	numericZone   = "-[0-9]{4}"                                   // -0300
-	milli         = ".[0-9]{3}"
-	micro         = ".[0-9]{6}"
-	nano          = ".[0-9]{9}"
-)
-
-var (
-	ANSICRegex 		 = regexp.MustCompile(fmt.Sprintf("%s %s %s %s %s", weekdaysAbv, monthsAbv, days, hhmmss,
-		year))
-	UnixDateRegex 	 = regexp.MustCompile(fmt.Sprintf("%s %s %s %s %s %s", weekdaysAbv, monthsAbv, days, hhmmss,
-		timezone, year))
-	RubyDateRegex 	 = regexp.MustCompile(fmt.Sprintf("%s %s %s %s %s %s", weekdaysAbv, monthsAbv, daysWithZero,
-		hhmmss, numericZone, year))
-	RFC822Regex 	 = regexp.MustCompile(fmt.Sprintf("%s %s [0-9]{2} %s %s", daysWithZero, monthsAbv, hhmm,
-		timezone))
-	RFC822ZRegex 	 = regexp.MustCompile(fmt.Sprintf("%s %s [0-9]{2} %s %s", daysWithZero, monthsAbv, hhmm,
-		numericZone))
-	RFC850Regex 	 = regexp.MustCompile(fmt.Sprintf("%s, %s-%s-[0-9]{2} %s %s", weekdays, daysWithZero,
-		monthsAbv, hhmmss, timezone))
-	RFC1123Regex 	 = regexp.MustCompile(fmt.Sprintf("%s, %s %s %s %s %s", weekdaysAbv, daysWithZero, monthsAbv,
-		year, hhmmss, timezone))
-	RFC1123ZRegex 	 = regexp.MustCompile(fmt.Sprintf("%s, %s %s %s %s %s", weekdaysAbv, daysWithZero, monthsAbv,
-		year, hhmmss, numericZone))
-	RFC3339Regex 	 = regexp.MustCompile(fmt.Sprintf("%s-%s-%sT%sZ%s", year, numericmonths, daysWithZero,
-		hhmmss, hhmm))
-	RFC3339NanoRegex = regexp.MustCompile(fmt.Sprintf("%s-%s-%sT%s%sZ%s", year, numericmonths, daysWithZero,
-		hhmmss, nano, hhmm))
-	KitchenRegex 	 = regexp.MustCompile("(([0-1]?[0-9]|2[0-3]):[0-5][0-9][P|A]M)")
-	StampRegex 		 = regexp.MustCompile(fmt.Sprintf("%s %s %s", monthsAbv, days, hhmmss))
-	StampMilliRegex  = regexp.MustCompile(fmt.Sprintf("%s %s %s%s", monthsAbv, days, hhmmss, milli))
-	StampMicroRegex  = regexp.MustCompile(fmt.Sprintf("%s %s %s%s", monthsAbv, days, hhmmss, micro))
-	StampNanoRegex   = regexp.MustCompile(fmt.Sprintf("%s %s %s%s", monthsAbv, days, hhmmss, nano))
-)
-
 var layoutRegexps = map[string]*regexp.Regexp{
-	time.ANSIC: 	  ANSICRegex,
-	time.UnixDate: 	  UnixDateRegex,
-	time.RubyDate: 	  RubyDateRegex,
-	time.RFC822: 	  RFC822Regex,
-	time.RFC822Z: 	  RFC822ZRegex,
-	time.RFC850: 	  RFC850Regex,
-	time.RFC1123: 	  RFC1123Regex,
-	time.RFC1123Z: 	  RFC1123ZRegex,
-	time.RFC3339: 	  RFC3339Regex,
-	time.RFC3339Nano: RFC3339NanoRegex,
-	time.Kitchen: 	  KitchenRegex,
-	time.Stamp: 	  StampRegex,
-	time.StampMilli:  StampMilliRegex,
-	time.StampMicro:  StampMicroRegex,
-	time.StampNano:   StampNanoRegex,
+	time.ANSIC:       ANSICRegexp,
+	time.UnixDate:    UnixDateRegexp,
+	time.RubyDate:    RubyDateRegexp,
+	time.RFC822:      RFC822Regexp,
+	time.RFC822Z:     RFC822ZRegexp,
+	time.RFC850:      RFC850Regexp,
+	time.RFC1123:     RFC1123Regexp,
+	time.RFC1123Z:    RFC1123ZRegexp,
+	time.RFC3339:     RFC3339Regexp,
+	time.RFC3339Nano: RFC3339NanoRegexp,
+	time.Kitchen:     KitchenRegexp,
+	time.Stamp:       StampRegexp,
+	time.StampMilli:  StampMilliRegexp,
+	time.StampMicro:  StampMicroRegexp,
+	time.StampNano:   StampNanoRegexp,
 }
 
 type MatcherFunc func(string) Match
@@ -112,7 +67,7 @@ func MatchRegexp(r *regexp.Regexp) MatcherFunc {
 }
 
 // MatchTimestamp returns a MatcherFunc that matches a time layout pattern in given string
-func MatchTimestamp(layout string) MatcherFunc{
+func MatchTimestamp(layout string) MatcherFunc {
 	return func(str string) Match {
 		r := layoutRegexps[layout]
 
