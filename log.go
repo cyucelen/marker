fb93fa5d6e6f25b4b5fc0c2269e8c8de5d9566f9
Cagatay Yucelen <cagatayyucelen@gmail.com>
2019-10-17 23:11:50 +0300

    io.Writer interface implemented instead of wrapping all logger methods

--- log.go
+++ log.go
@@ -1,52 +1,51 @@
 package marker
 
 import (
-	"fmt"
-	"log"
+	"io"
 	"os"
 
 	"github.com/fatih/color"
 )
 
-type LoggerOption func(*Logger)
+type StdoutMarkerOption func(*StdoutMarker)
 
 type MarkRule struct {
 	Matcher MatcherFunc
 	Color   *color.Color
 }
 
-type Logger struct {
-	rules  []MarkRule
-	logger *log.Logger
+type StdoutMarker struct {
+	rules []MarkRule
+	out   io.Writer
 }
 
-func NewLogger(options ...LoggerOption) *Logger {
-	logMarker := &Logger{logger: log.New(os.Stdout, "", 0)}
+func NewStdoutMarker(options ...StdoutMarkerOption) *StdoutMarker {
+	logMarker := &StdoutMarker{out: os.Stdout}
 	for _, option := range options {
 		option(logMarker)
 	}
 	return logMarker
 }
 
-func SetLogger(logger *log.Logger) LoggerOption {
-	return func(logMarker *Logger) {
-		logMarker.logger = logger
-	}
+func (s *StdoutMarker) AddRule(rule MarkRule) *StdoutMarker {
+	s.rules = append(s.rules, rule)
+	return s
 }
 
-func (l *Logger) AddRule(rule MarkRule) *Logger {
-	l.rules = append(l.rules, rule)
-	return l
+func (s *StdoutMarker) AddRules(rules []MarkRule) {
+	s.rules = append(s.rules, rules...)
 }
 
-func (l *Logger) AddRules(rules []MarkRule) {
-	l.rules = append(l.rules, rules...)
-}
-
-func (l *Logger) Print(v ...interface{}) {
-	marked := fmt.Sprint(v...)
-	for _, rule := range l.rules {
+func (s StdoutMarker) Write(p []byte) (n int, err error) {
+	marked := string(p)
+	for _, rule := range s.rules {
 		marked = Mark(marked, rule.Matcher, rule.Color)
 	}
-	l.logger.Print(marked)
+	return s.out.Write([]byte(marked))
+}
+
+func SetOutput(out io.Writer) StdoutMarkerOption {
+	return func(stdoutWriter *StdoutMarker) {
+		stdoutWriter.out = out
+	}
 }
--- log_test.go
+++ log_test.go
@@ -20,49 +20,47 @@ func (m *MockLogOut) Write(p []byte) (n int, err error) {
 }
 
 func Test_New(t *testing.T) {
-	logMarker := NewLogger()
-	assert.NotNil(t, logMarker.logger)
-
-	expectedLogger := log.New(os.Stdout, "test", 0)
-	logMarkerWithLogger := NewLogger(SetLogger(expectedLogger))
-	assert.Equal(t, expectedLogger, logMarkerWithLogger.logger)
+	stdoutMarker := NewStdoutMarker()
+	assert.NotNil(t, stdoutMarker.out)
+	assert.Equal(t, stdoutMarker.out, os.Stdout)
 }
 
 func Test_AddRule(t *testing.T) {
-	logger := NewLogger()
+	stdoutMarker := NewStdoutMarker()
 	markRule := MarkRule{MatchAll("test"), color.New(color.FgRed)}
-	logger.AddRule(markRule)
-	assert.Len(t, logger.rules, 1)
+	stdoutMarker.AddRule(markRule)
+	assert.Len(t, stdoutMarker.rules, 1)
 
 	markRule = MarkRule{MatchAll("want"), color.New(color.FgBlue)}
-	logger.AddRule(markRule)
-	assert.Len(t, logger.rules, 2)
+	stdoutMarker.AddRule(markRule)
+	assert.Len(t, stdoutMarker.rules, 2)
 }
 
 func Test_AddRules(t *testing.T) {
-	logger := NewLogger()
+	stdoutMarker := NewStdoutMarker()
 	markRules := []MarkRule{
 		{MatchBracketSurrounded(), color.New(color.FgGreen)},
 		{MatchParensSurrounded(), color.New(color.BgBlack)},
 		{MatchEmail(), color.New(color.FgCyan)},
 	}
-	logger.AddRules(markRules)
-	assert.Len(t, logger.rules, 3)
+	stdoutMarker.AddRules(markRules)
+	assert.Len(t, stdoutMarker.rules, 3)
 
-	logger.AddRules(markRules[:2])
-	assert.Len(t, logger.rules, 5)
+	stdoutMarker.AddRules(markRules[:2])
+	assert.Len(t, stdoutMarker.rules, 5)
 }
 
-func Test_Print(t *testing.T) {
+func Test_Write(t *testing.T) {
 	redFg := color.New(color.FgRed)
 	redFg.EnableColor()
 	red := redFg.SprintFunc()
 
 	mockOut := &MockLogOut{}
-	logger := NewLogger(SetLogger(log.New(mockOut, "", 0)))
+	stdoutMarker := NewStdoutMarker(SetOutput(mockOut))
 
-	logger.AddRule(MarkRule{MatchAll("skydome"), redFg}).AddRule(MarkRule{MatchAll("data"), redFg})
+	stdoutMarker.AddRule(MarkRule{MatchAll("skydome"), redFg}).AddRule(MarkRule{MatchAll("data"), redFg})
 
+	logger := log.New(stdoutMarker, "", 0)
 	logger.Print("best data company is skydome")
 
 	expectedLog := fmt.Sprintf("best %s company is %s\n", red("data"), red("skydome"))
